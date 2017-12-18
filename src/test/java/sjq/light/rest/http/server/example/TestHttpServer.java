28aea0e6d009fa4da9f5b3e741f2b38c31c8c6d8
jackie.sjq <jackie.sjq@alibaba-inc.com>
2017-12-18 11:16:13 +0800

    modifier the main method

--- src/test/java/sjq/light/rest/http/server/example/TestHttpServer.java
+++ src/test/java/sjq/light/rest/http/server/example/TestHttpServer.java
@@ -1,13 +1,14 @@
 package sjq.light.rest.http.server.example;
 
-import org.junit.Test;
-
 import sjq.light.rest.http.server.HttpRestServer;
 
 public class TestHttpServer {
 
-	@Test
-	public void testHttpRestServer() throws Exception {
+	public static void main(String[] args) throws Exception {
+		testHttpRestServer();
+	}
+	
+	public static void testHttpRestServer() throws Exception {
 		try (HttpRestServer httpRestServer = new HttpRestServer(8080)) {
 			httpRestServer.scanRestPackage("sjq.light.rest.http.server.example");
 			httpRestServer.start();
